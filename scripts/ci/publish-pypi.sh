2c5840bda9175c49dbdb8cf1281dc68fe7db437e
Varchas Gopalaswamy <vgop@lle.rochester.edu>
2026-06-06 13:24:25 -0400

    clean CI

--- scripts/ci/publish-pypi.sh
+++ scripts/ci/publish-pypi.sh
@@ -8,6 +8,7 @@ fi
 
 export UV_SYSTEM_PYTHON="${UV_SYSTEM_PYTHON:-1}"
 
+rm -rf dist
 uv sync
 uv run --with build python -m build
 uv run --with twine twine upload --verbose -u __token__ -p "${PYPI_TOKEN}" dist/*
