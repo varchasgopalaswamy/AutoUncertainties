3c71d8dc9feec9a651316cda4d39ec5042f3839c
Varchas Gopalaswamy <vgop@lle.rochester.edu>
2026-06-06 12:41:37 -0400

    updated CI

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -6,6 +6,7 @@ export UV_SYSTEM_PYTHON="${UV_SYSTEM_PYTHON:-1}"
 uv sync --extra CI
 
 # Run ruff to check for linting issues
-uv run ruff check .
+uv tool install ruff
+ruff check .
 
 uv run coverage run -m pytest --cov --cov-report=xml --cov-report=html --cov-report=term
